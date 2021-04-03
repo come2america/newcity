a66ae4a97283aff1d9d39bc9d38e28fa7e2b56b9
Yusuf Serunjogi <ysserunjogi@gmail.com>
2021-04-02 23:54:02 -0400

    Update app2.js

--- Assets/JavaScript/app2.js
+++ Assets/JavaScript/app2.js
@@ -142,8 +142,8 @@ $("#submit").on("click", function (event) {
     ref.orderByChild("userstreet").equalTo(street).on("value", function (snapshot) {
         snapshot.forEach(function (childSnapshot) {
             console.log(childSnapshot)
-            var value = childSnapshot.val();
-            assigntrack = value.usertrackid;
+        //    var value = childSnapshot.val();
+        //    assigntrack = value.usertrackid;
         });
 
         if (snapshot.val()) {
@@ -206,15 +206,15 @@ $("#submit").on("click", function (event) {
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("submit");
+//var btn = document.getElementById("submit");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+//btn.onclick = function() {
+//  modal.style.display = "block";
+//}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
